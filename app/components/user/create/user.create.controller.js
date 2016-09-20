e39c219f7cf1e0f7712b7151e973ab266bf90b8d
Rastelli <felipe-rastelli@hotmail.com>
2016-09-20 03:44:39 -0300

    User model defined.

--- app/components/user/create/user.create.controller.js
+++ app/components/user/create/user.create.controller.js
@@ -1,11 +1,46 @@
 (function(){
   'use strict';
 
-  const UserCreateController = function($scope){
+  const UserCreateController = function($scope, $state){
+    var vm = this;
 
+    vm.user = {
+      nome: '',
+      sobrenome: '',
+      cpf: '',
+      telefone: '',
+      endereco: {
+        logradouro: '',
+        numero: '',
+        cep: '',
+        bairro: '',
+        cidade: '',
+        uf: ''
+      },
+      account: { 
+        user: '', 
+        password: '' 
+      }
+    };
+
+    
+
+    
+
+    vm.submit = Submit;
+    vm.cancel = Cancel;
+
+    function Submit(){
+
+    }
+
+    function Cancel(){
+      vm.user = {};
+      $state.go('home');
+    }
   };
 
-  UserCreateController.$inject = ['$scope'];
+  UserCreateController.$inject = ['$scope', '$state'];
 
   angular.module('user.module').controller('UserCreateController', UserCreateController);
 })();
