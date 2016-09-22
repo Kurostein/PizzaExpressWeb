2e73a07b33b5def00a13cc28712385cf071d9f0a
Rastelli <felipe-rastelli@hotmail.com>
2016-09-22 03:30:13 -0300

    temp socket server added.

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+http.createServer(app).listen(3030, function(){
+  console.log('Servidor online!');
+  console.log(__dirname);
+});
